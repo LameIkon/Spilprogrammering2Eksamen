ee3159fa60919c746049ff515bb55dbe11b454a9
4sela <adawa21@student.sdu.dk>
2024-05-09 02:16:01 +0200

    Added GlobalScore Text

--- Assets/_Scripts/SaveTester.cs
+++ Assets/_Scripts/SaveTester.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveTester : MonoBehaviour
-{
-
-}
+// public class SaveTester : MonoBehaviour
+// {
+//
+// }
