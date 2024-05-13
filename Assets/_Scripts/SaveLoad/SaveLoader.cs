2dddfb36c3815122630073c7329637a1897c2684
LameIkon <troelsjelsbak@hotmail.com>
2024-05-13 13:08:11 +0200

    Comment

--- Assets/_Scripts/SaveLoad/SaveLoader.cs
+++ Assets/_Scripts/SaveLoad/SaveLoader.cs
@@ -3,12 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using Mirror;
 
 public class SaveLoader : MonoBehaviour
 {
-
-
     private string _dataPath;
     public string _fileName;
     private const string _fileExtention = ".json";
@@ -20,7 +17,7 @@ public class SaveLoader : MonoBehaviour
     private void Awake() 
     {
         _playerMovement = GetComponent<PlayerMovement>();
-        _dataPath = Application.persistentDataPath;     // This will be saved in the Appdata Folder
+        _dataPath = Application.persistentDataPath;     // This will be saved in the AppData Folder
         _serializer = new JsonSerializer();
         _playerData = new PlayerData();
     }
@@ -29,34 +26,34 @@ public class SaveLoader : MonoBehaviour
     {
         _fileName = _playerMovement._Name;
 
-        if (File.Exists(GetFilePath(_fileName)))
+        if (File.Exists(GetFilePath(_fileName)))    // Loading the file if it exists.
         {
             Load(_fileName);
         }
         else 
         {
-            Save(_fileName);
+            Save(_fileName);    // Making a save, such that we know that a PlayerData exists, such that we can save data into it.
         }
     }
 
     private void Update() 
     {
-        _playerData._Position = transform.position;
+        _playerData._Position = transform.position;     // Setting the transform in the PlayerData
         _playerData._Rotation = transform.rotation;
-        _playerData._Velocity = _playerMovement._Rb.velocity;
-        Save(_fileName );
+        _playerData._Velocity = _playerMovement._Rb.velocity;   // Setting the velocity in the PlayerData
+        Save(_fileName);
     }
 
     private string GetFilePath(string fileName) 
     {
-        return Path.Combine(_dataPath, string.Concat(fileName, _fileExtention));
+        return Path.Combine(_dataPath, string.Concat(fileName, _fileExtention)); // Here we get the file path, so we can get the files.
     }
 
     public void Save(string fileName) 
     {
-        string fileLocation = GetFilePath(fileName);
+        string fileLocation = GetFilePath(fileName);    // Get the fileLocation
 
-        File.WriteAllText(fileLocation, _serializer.Serialize(_playerData));
+        File.WriteAllText(fileLocation, _serializer.Serialize(_playerData));    // Serialize the data and write the text.
     }
 
     public void Load(string fileName) 
